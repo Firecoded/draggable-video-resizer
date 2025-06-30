a7cbbc3b8e2896a74d0ffb50f85dc8811071cdb4
Jacob Taylor <yepyepguru@gmail.com>
2025-06-29 18:15:04 -0700

    add install page with confetti

--- public/background.js
+++ public/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install") {
-    chrome.tabs.create({ url: chrome.runtime.getURL("instructions.html") });
+    chrome.tabs.create({ url: chrome.runtime.getURL("instructions.html?install=true") });
   }
 });
